cfc5cedb17e6ec0cb6fb8d2774e040ca20767398
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-08-04 13:35:36 +0530

    CPP

---
+++ 1_Type Casting/hlo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+#include<math.h>
+using namespace std;
+ 
+int main() {
+    int num1  = 5;
+    float num2 = 5.5f;
+
+    int result = num1 + num2;
+    cout << " Result is : " << result << endl;
+
+
+
+    char ch = 'A';
+    char num3 = 5;
+
+    int result1 = ch + num3;
+    cout << "Result is : " << result1 << endl;
+
+    
+    return 0;
+}
