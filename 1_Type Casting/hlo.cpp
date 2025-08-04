a989f43f1a2bc7aae9ee70796b09a4222eddedfa
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-08-04 14:00:08 +0530

    CPP

--- 1_Type Casting/hlo.cpp
+++ 1_Type Casting/hlo.cpp
@@ -6,6 +6,8 @@
 using namespace std;
  
 int main() {
+
+    // implicit type conversion
     int num1  = 5;
     float num2 = 5.5f;
 
@@ -18,8 +20,16 @@ int main() {
     char num3 = 5;
 
     int result1 = ch + num3;
-    cout << "Result is : " << result1 << endl;
+    cout << "Result1 is : " << result1 << endl;
 
     
+
+    // Explicit type conversion
+    int num4 = 10;
+    int num5 = 3;
+
+    float result2 = (float)num4 / num5;
+
+    cout << "Result2 is : " << result2 << endl;
     return 0;
 }
